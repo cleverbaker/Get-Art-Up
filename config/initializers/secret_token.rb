7960ecdbd8062cd85ba968951454ab162ca9bf93
Cory Logan <cory@counterbeing.com>
2011-11-19 10:10:31 -0800

    created rails 3_1 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GetArtUp::Application.config.secret_token = '5f46f3f8621e317b87199dc55ec25c1cd652688685ebf52965d973635e94f8e3f2848ddeb411d3fb37e0155ecb2ac6a09ef28a9d0af0ad168e3ff68a0e2d0c15'
